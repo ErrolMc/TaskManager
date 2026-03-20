7e1aa9a8ac755963f19ef94c25c7b99a2adf7943
Errol <errolmcfadden@hotmail.com>
2026-03-20 11:50:40 +1000

    - Missed file from last commit

---
+++ TaskManager.Backend/Contracts/Notifications/ColumnCreatedNotification.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManager.Backend.Contracts.Notifications
+{
+    public class ColumnCreatedNotification : Notification
+    {
+        public override NotificationType NotificationType => NotificationType.ColumnCreated;
+
+        public string BoardID { get; set; } = string.Empty;
+        public string ColumnID { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+    }
+}
